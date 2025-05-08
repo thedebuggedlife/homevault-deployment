276d3222748ff4fe8fb3585977f0e3325a3618a0
Antonio Vargas <antonio.vargas@outlook.com>
2025-05-08 13:02:47 -0700

    UPNP script improvements
    - Unregister other hosts linked to the TURN port
    - Better logging

--- appdata/nextcloud.talk/nextcloud/scripts/turn-upnpc.sh
+++ appdata/nextcloud.talk/nextcloud/scripts/turn-upnpc.sh
@@ -1,37 +1,86 @@
 #!/usr/local/bin/bash
 
+exec 3>&2
+
+exec 2> >(while read -r line; do echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR $line" >&3; done)
+
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") ${2:-INFO} $1"
+}
+
+if [ "$UPNP_SLEEP" = "0" ]; then
+    log "UPnP port mapping is disabled via environment variable"
+    sleep infinite
+fi
+
 if [ -z "$HOST_PORT" ]; then
-    echo "Missing variable: HOST_PORT" >&2
+    log "Missing variable: HOST_PORT" ERROR
     exit 1
 fi
 
 if [ -z "$ROUTER_PORT" ]; then
-    echo "Missing variable: ROUTER_PORT" >&2
+    log "Missing variable: ROUTER_PORT" ERROR
     exit 1
 fi
 
 if ! apk info -e "miniupnpc" >/dev/null 2>&1; then
-    echo "Installing miniupnpc..."
+    log "Installing miniupnpc..."
     if ! apk add "miniupnpc"; then
-        echo "Failed to install miniupnpc" >&2
+        log "Failed to install miniupnpc" ERROR
         exit 1
     fi
 fi
 
-upnpc_args=(-e "Nextcloud Talk TURN Server" -r "${HOST_PORT}" "${ROUTER_PORT}" tcp "${HOST_PORT}" "${ROUTER_PORT}" udp)
-
-while true; do
+call_upnpc() {
     # upnpc can be flaky - try 3 times before giving up
-    upnpc "${upnpc_args[@]}" || {
-        echo "Call to upnpc failed. Trying again..." >&2
-        upnpc "${upnpc_args[@]}" || {
-            echo "Call to upnpc failed. Trying again..." >&2
-            upnpc "${upnpc_args[@]}" || {
-                echo "Call to upnpc failed. Giving up." >&2
+    upnpc "$@" >/dev/null || {
+        log "Call to upnpc failed. Trying again..." WARN
+        upnpc "$@" >/dev/null || {
+            log "Call to upnpc failed. Trying again..." WARN
+            upnpc "$@" >/dev/null || {
+                log "Call to upnpc failed. Giving up." ERROR
+                return 1
             }
         }
     }
-    date
-    echo "Sleeping for ${UPNP_SLEEP:-3600} seconds"
+}
+
+get_current_ip() {
+    ip -o -4 route get 1 | awk '{print $7}'
+}
+
+add_my_mappings() {
+    log "Adding port mappings for ${ROUTER_PORT}:TCP/UDP to this host"
+    call_upnpc -e "Nextcloud Talk TURN Server" -r "${HOST_PORT}" "${ROUTER_PORT}" TCP "${HOST_PORT}" "${ROUTER_PORT}" UDP || return 1
+}
+
+clear_mapping() {
+    local remote_host=$1
+    local protocol=$2
+    log "Removing port mappings for ${ROUTER_PORT}:$protocol to host $remote_host"
+    call_upnpc -d "${ROUTER_PORT}" "$protocol" "$remote_host"
+}
+
+clear_other_mappings() {
+    local line remote_host my_host protocol
+    my_host=$(get_current_ip)
+    upnpc -l | grep "${ROUTER_PORT}->" | while read -r line; do
+        if [[ $line =~ ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) ]]; then
+            remote_host="${BASH_REMATCH[1]}"
+            if [ "$remote_host" != "$my_host" ]; then
+                if [[ $line =~ (TCP|UDP)\b[[:space:]] ]]; then
+                    protocol="${BASH_REMATCH[1]}"
+                    clear_mapping "$remote_host" "$protocol"
+                fi
+            fi
+        fi
+    done
+}
+
+while true; do
+    log "Starting new run ..."
+    clear_other_mappings
+    add_my_mappings
+    log "Sleeping for ${UPNP_SLEEP:-3600} seconds ..."
     sleep "${UPNP_SLEEP:-3600}"
 done
