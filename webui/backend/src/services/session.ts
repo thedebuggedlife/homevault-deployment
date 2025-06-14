8cd93c2e2f60022491217ae26d78e15fb8fc7d5f
Antonio Vargas <antonio.vargas@outlook.com>
2025-06-14 08:56:41 -0700

    Add a session-level cache for sudo passwords

--- webui/backend/src/services/session.ts
+++ webui/backend/src/services/session.ts
@@ -4,11 +4,13 @@ import { Namespace, Socket } from "socket.io";
 import { v4 as uuid } from "uuid";
 import { logger as rootLog } from "@/logger";
 import _ from "lodash";
+import { SudoCache } from "@/util/sudoCache";
 
 export type SessionSocket = Socket<SessionClientEvents, SessionServerEvents>;
 
 class Session {
     private readonly logger;
+    private sudoCache = new SudoCache();
 
     constructor(sessionId: string, private socket: SessionSocket) {
         this.logger = rootLog.child({ source: "Session", sessionId })
@@ -17,17 +19,30 @@ class Session {
 
     async askSudo(username: string, timeoutMs = 90000) {
         this.logger.info("Requesting sudo password", { username, timeoutMs });
+        const cached = this.sudoCache.get(username);
+        if (!_.isEmpty(cached)) {
+            this.logger.info("Using cached sudo password for " + username);
+            return cached;
+        }
         try {
             const request = {
                 username,
                 timeoutMs
             }
             const { password } = await this.socket.timeout(timeoutMs).emitWithAck("sudo", request);
-            return password;    
+            if (!_.isEmpty(password)) {
+                this.logger.info("Caching sudo password for " + username);
+                this.sudoCache.set(username, password!);
+            }
+            return password;
         } catch (error) {
             this.logger.warn("Sudo password request timed out", { timeoutMs });
             throw error;
         }
+    }
+
+    close() {
+        this.sudoCache.clear();
     }
 }
 
@@ -42,6 +57,7 @@ class SessionManager {
         this.logger.info("Session connected", { sessionId });
         socket.on("disconnect", (reason) => {
             this.logger.info("Session disconnected", { sessionId, reason });
+            this.connections[sessionId]?.close();
             delete this.connections[sessionId];
         });
         this.connections[sessionId] = new Session(sessionId, socket);
